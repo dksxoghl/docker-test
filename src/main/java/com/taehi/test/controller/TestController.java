dfc678d1051874cb5de232c8eb45eb80bd6068e6
dksxoghl <dksxoghl8@gmail.com>
2020-10-03 17:25:24 +0900

    first commit

---
+++ src/main/java/com/taehi/test/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.taehi.test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello, Docker!";
+    }
+}
