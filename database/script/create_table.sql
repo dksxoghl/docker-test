752c82295c50bc182cf679b09273e4a8c5e600dc
dksxoghl <dksxoghl8@gmail.com>
2020-10-22 20:01:19 +0900

    CI CD Test

---
+++ database/script/create_table.sql
@@ -0,0 +1,4 @@
+create table hello(
+ h_id varchar(10) not null,
+ h_test varchar(10)
+)
---
+++ database/script/insert_data.sql
@@ -0,0 +1,2 @@
+insert into hello (h_id,h_test) values ('a','b');
+insert into hello (h_id,h_test) values ('c','d');
