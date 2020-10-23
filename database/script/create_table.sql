dc312e991b5dd758284f58bdfa39d1e3b66bbd07
dksxoghl <dksxoghl8@gmail.com>
2020-10-23 16:43:29 +0900

    CI CD Test

--- database/script/create_table.sql
+++ database/script/create_table.sql
@@ -1,4 +1,4 @@
-create table hello(
+create table IF NOT EXISTS hello(
  h_id varchar(10) not null,
  h_test varchar(10)
 )
--- src/main/java/com/taehi/test/controller/TestController.java
+++ src/main/java/com/taehi/test/controller/TestController.java
@@ -1,13 +1,19 @@
 package com.taehi.test.controller;
 
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class TestController {
+import javax.sql.DataSource;
 
+@RestController
+public class TestController extends JdbcDaoSupport {
+
+    public TestController(DataSource dataSource) {
+        setDataSource(dataSource);
+    }
     @RequestMapping("/")
     public String hello() {
-        return "Hello, Docker! 23롤링?";
+        return getJdbcTemplate().queryForObject("select h_test from hello where h_id=?", String.class, "a");
     }
 }
